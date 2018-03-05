5af1be2dcc79ed9b1f92d0b7ab9c03d37c305395
Wictor Wilen <wictor@wictorwilen.se>
2018-03-05 10:10:34 -0800

    Switched to correct license: MIT

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,7 @@
+// Copyright (c) Wictor Wilén. All rights reserved. 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 import {GeneratorTeamsApp} from './GeneratorTeamsApp';
 
 module.exports = GeneratorTeamsApp
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+// Copyright (c) Wictor Wilén. All rights reserved. 
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 var webpack = require('webpack');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
