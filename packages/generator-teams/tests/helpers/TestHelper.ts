a1dc52374456f822e5ecd1c57e2cd8aa48f8c585
Stephan Bisser <stephan@bisser.at>
2021-09-21 16:31:30 +0200

    [UPDATE] Added support for manifest version 11

--- packages/generator-teams/tests/helpers/TestHelper.ts
+++ packages/generator-teams/tests/helpers/TestHelper.ts
@@ -120,6 +120,7 @@ export async function runNpmCommand(command: string, path: string): Promise<bool
 export const SCHEMA_18 = 'https://developer.microsoft.com/en-us/json-schemas/teams/v1.8/MicrosoftTeams.schema.json';
 export const SCHEMA_19 = 'https://developer.microsoft.com/en-us/json-schemas/teams/v1.9/MicrosoftTeams.schema.json';
 export const SCHEMA_110 = 'https://developer.microsoft.com/en-us/json-schemas/teams/v1.10/MicrosoftTeams.schema.json';
+export const SCHEMA_111 = 'https://developer.microsoft.com/en-us/json-schemas/teams/v1.11/MicrosoftTeams.schema.json';
 export const SCHEMA_DEVPREVIEW = 'https://raw.githubusercontent.com/OfficeDev/microsoft-teams-app-schema/preview/DevPreview/MicrosoftTeams.schema.json';
 
 export const INTEGRATION_TEST_VERSIONS = ["v1.9", "v1.10"]; // only keep two versions, so we can stay under Github 360 minute rule
@@ -128,14 +129,16 @@ export const SCHEMAS: { [key: string]: string } = {
   "v1.8": SCHEMA_18,
   "v1.9": SCHEMA_19,
   "v1.10": SCHEMA_110,
+  "v1.11": SCHEMA_111,
   "devPreview": SCHEMA_DEVPREVIEW
 }
 
 // All the paths for upgrading
 const UPGRADE_PATHS: { [key: string]: string[] } = {
   "v1.8": ["v1.9", "v1.10", "devPreview"],
-  "v1.9": ["v1.10", "devPreview"],
-  "v1.10": ["devPreview"]
+  "v1.9": ["v1.10", "v1.11", "devPreview"],
+  "v1.10": ["v1.11", "devPreview"],
+  "v1.11": ["devPreview"]
 }
 
 export enum TestTypes {
