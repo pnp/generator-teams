e23d39525a3861fac9f151f1c8b26701d09f6e65
Wictor Wilén <wictor@wictorwilen.se>
2021-11-15 14:35:09 +0100

    ⬆️ Upgraded dependency to msteams-react-base-component

--- packages/generator-teams/src/app/GeneratorTeamsApp.ts
+++ packages/generator-teams/src/app/GeneratorTeamsApp.ts
@@ -615,7 +615,7 @@ export class GeneratorTeamsApp extends Generator {
         // if we have added any react based components
         if (this.options.reactComponents) {
             Yotilities.addAdditionalDeps([
-                ["msteams-react-base-component", "^4.0.0-preview"]
+                ["msteams-react-base-component", "^4.0.0-preview3"]
             ], this.fs);
         }
 
